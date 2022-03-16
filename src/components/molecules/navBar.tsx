c4b404d1ff08762d7cd47ed7dc6e041daa2bdd00
sajalkhan <sajalhossen@yahoo.com>
2022-03-16 19:21:18 +0600

    update class name

--- src/components/molecules/navBar.tsx
+++ src/components/molecules/navBar.tsx
@@ -8,19 +8,19 @@ export interface navbarProps {
 const NavBar: React.FC<navbarProps> = ({ userName }) => {
   return (
     <div className="m-navbar">
-      <div className="m-navbar__wrapper">
-        <div className="m-navbar__wrapper--logo">
+      <div className="m-navbar__Left">
+        <div className="m-navbar__Left--logo">
           <Image src="/static/images/netflix.svg" alt="Netflix logo" width="128px" height="34px" />
         </div>
-        <ul className="m-navbar__wrapper--items">
-          <li className="m-navbar__wrapper--items-home">Home</li>
-          <li className="m-navbar__wrapper--items-myList">My List</li>
+        <ul className="m-navbar__Left--items">
+          <li className="m-navbar__Left--items-home">Home</li>
+          <li className="m-navbar__Left--items-myList">My List</li>
         </ul>
       </div>
 
-      <nav className="m-navbar__container">
+      <nav className="m-navbar__Right">
         <Button modifiers="text" title={userName}></Button>
-        <div className="m-navbar__container--dropdown">
+        <div className="m-navbar__Right--dropdown">
           <a>Sign out</a>
         </div>
       </nav>
