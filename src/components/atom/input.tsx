7c57b74ce50e37f29d42e35c2ef0edce6d2b6139
sajalkhan <sajalhossen@yahoo.com>
2022-03-20 07:59:47 +0600

    fix interface name

--- src/components/atom/input.tsx
+++ src/components/atom/input.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export interface inputTextProps {
+export interface inputProps {
   placeholder?: string;
   type?: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Input: React.FC<inputTextProps> = ({ placeholder, type = 'text', onChange }) => {
+const Input: React.FC<inputProps> = ({ placeholder, type = 'text', onChange }) => {
   return <input className="a-input" type={type} placeholder={placeholder} onChange={onChange} />;
 };
 
